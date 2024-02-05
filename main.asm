81e18c25649ba66e20c06f117078508aea1b7293
kangsiwoo <07rkdtldn@gmail.com>
2024-02-06 01:18:27 +0900

    base of Variable

--- main.asm
+++ main.asm
@@ -1,14 +1,17 @@
 main:
     ; 호출
-    pushq %rbp
-    movq %rsp, %rbp
-    subq $32, %rsp
-    call __main
+    pushq   %rbp
+    movq    %rsp, %rbp
+    subq    $32, %rsp
+    call    __main
+
+    ;변수 선언
+    movb    $0, -1(%rbp)
 
     ; 반환값
-    movl $0, %eax
+    movl    $0, %eax
 
     ; 끝
-    addq $32, %rsp
-    popq %rbp
+    addq    $32, %rsp
+    popq    %rbp
     ret
